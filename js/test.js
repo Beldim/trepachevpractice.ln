9b5dc5da8f6239eba816cde80a4b4cdb7253343d
Beldim <dimonja@inbox.ru>
2020-06-30 15:51:55 +0300

    practice 1

--- js/test.js
+++ js/test.js
@@ -1,2 +1,13 @@
 ﻿/* practice */
 /* 1 */
+/* let elem = document.getElementById('button');
+elem.addEventListener('click', go);
+let input = document.getElementsByClassName('input');
+function go(){
+    let sum = 0;
+    for (i = 0; i < input.length; i++){
+        sum += +input[i].value;
+    }
+    let result = document.getElementById('sum');
+    result.value = sum;
+} */
