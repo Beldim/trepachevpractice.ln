11887f2e5968ce5572bfd5158380ddda6769e8ed
Beldim <dimonja@inbox.ru>
2020-07-06 16:02:00 +0300

    Tasks for working with page elements in JavaScript

--- js/test.js
+++ js/test.js
@@ -11,3 +11,40 @@ function go(){
     let result = document.getElementById('sum');
     result.value = sum;
 } */
+/* 2 */
+/* let elem = document.getElementById('button');
+elem.addEventListener('click', go);
+let num = document.getElementsByClassName('num');
+function go(){
+    let sum = 0;
+    for (i = 0; i < num.length; i ++){
+        sum += +num[i].value;
+    }
+    let result = document.getElementById('result');
+    result.innerHTML = sum;
+} */
+/* 3 */
+/* let num = document.getElementById('num');
+num.addEventListener('blur', go);
+function go(){
+    let arr = num.value.split('');
+    let sum = 0;
+    for (i=0; i<arr.length; i++){
+        sum += +arr[i];
+    }
+    let result = document.getElementById('result');
+    result.innerHTML = sum;
+} */
+/* 4 */
+/* let num = document.getElementById('num');
+num.addEventListener('blur', go);
+function go(){
+    let arr = num.value.split(',');
+    let sum = 0;
+    for (i=0; i<arr.length; i++){
+        sum += +arr[i];
+    }
+    let result = document.getElementById('result');
+    result.innerHTML = sum/arr.length;
+} */
+/* 5 */
